ed70995f677823ddce4ae43da6330b9e5a1d9dc2
RAINFOREST\Gustavo <gustavomonardez@gmail.com>
2018-04-04 15:39:29 -0700

    Initial Solution Setup

---
+++ E-Commerce/E-Commerce.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Commerce.WebUI.Startup))]
+namespace E_Commerce.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
