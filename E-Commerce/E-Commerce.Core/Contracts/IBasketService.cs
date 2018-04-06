a51840684706678cb81bcecaa43e09baf0d7b5bc
RAINFOREST\Gustavo <gustavomonardez@gmail.com>
2018-04-06 11:33:30 -0700

    Finish Basket Service

---
+++ E-Commerce/E-Commerce.Core/Contracts/IBasketService.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace E_Commerce.Core.Contracts {
+
+    public interface IBasketService {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+    }
+}
